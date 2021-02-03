031deca17c910b6371e2be94eb73620d98fc54ff
alex <aleksandr.sevcenko@nordsec.com>
2021-02-04 01:10:27 +0200

    ...

--- src/perfect6502.h
+++ src/perfect6502.h
@@ -1,26 +1,25 @@
-#ifndef INCLUDED_FROM_NETLIST_SIM_C
-#define state_t void
-#endif
+
+struct state_t;
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-state_t *initAndResetChip();
-void destroyChip(state_t *state);
-void step(state_t *state);
-void chipStatus(state_t *state);
-unsigned short readPC(state_t *state);
-unsigned char readA(state_t *state);
-unsigned char readX(state_t *state);
-unsigned char readY(state_t *state);
-unsigned char readSP(state_t *state);
-unsigned char readP(state_t *state);
-unsigned int readRW(state_t *state);
-unsigned short readAddressBus(state_t *state);
-void writeDataBus(state_t *state, unsigned char);
-unsigned char readDataBus(state_t *state);
-unsigned char readIR(state_t *state);
+struct state_t *initAndResetChip();
+void destroyChip(struct state_t *state);
+void step(struct state_t *state);
+void chipStatus(struct state_t *state);
+unsigned short readPC(struct state_t *state);
+unsigned char readA(struct state_t *state);
+unsigned char readX(struct state_t *state);
+unsigned char readY(struct state_t *state);
+unsigned char readSP(struct state_t *state);
+unsigned char readP(struct state_t *state);
+unsigned int readRW(struct state_t *state);
+unsigned short readAddressBus(struct state_t *state);
+void writeDataBus(struct state_t *state, unsigned char);
+unsigned char readDataBus(struct state_t *state);
+unsigned char readIR(struct state_t *state);
 
 extern unsigned char memory[65536];
 extern unsigned int cycle;
