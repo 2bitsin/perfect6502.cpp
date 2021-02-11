26b6b28ea8f5935623cb691c9fc15b4373d1778d
alex <aleksandr.sevcenko@nordsec.com>
2021-02-11 21:55:20 +0200

    ...

--- src/netlist_6502.cpp
+++ src/netlist_6502.cpp
@@ -43,7 +43,7 @@ struct netlist_6502_static_state_type
 	count_t	nodes_c1c2offset [netlist_6502_node_count + 1];
 	std::pair<nodenum_t, nodenum_t> nodes_c1c2s [netlist_6502_transistor_count * 2];
 	array_list<nodenum_t, netlist_6502_node_count> nodes_gates [netlist_6502_node_count];
-	array_list<nodenum_t, netlist_6502_node_count> nodes_dependant [2][netlist_6502_node_count];	
+	array_list<nodenum_t, netlist_6502_node_count> nodes_dependant [2][netlist_6502_node_count];
 
 	constexpr netlist_6502_static_state_type ()
 	{
@@ -51,17 +51,17 @@ struct netlist_6502_static_state_type
 
 		count_t c1c2count [netlist_6502_node_count];
 
-		for (auto& list : nodes_gates						)	list.clear ();
-		for (auto& list : nodes_dependant[0]		)	list.clear ();
-		for (auto& list : nodes_dependant[1]		)	list.clear ();
-		for (auto& node : nodes_c1c2offset			)	node = 0;
-		for (auto& node : c1c2count							) node = 0;
-				
+		for (auto& list : nodes_gates)	list.clear ();
+		for (auto& list : nodes_dependant [0])	list.clear ();
+		for (auto& list : nodes_dependant [1])	list.clear ();
+		for (auto& node : nodes_c1c2offset)	node = 0;
+		for (auto& node : c1c2count) node = 0;
+
 		for (auto&& tindex : range (0, netlist_6502_transistor_count))
 		{
 			c1c2count [netlist_6502_transdefs [tindex].c1]++;
 			c1c2count [netlist_6502_transdefs [tindex].c2]++;
-		}			
+		}
 
 		/* then sum the counts to find each node's offset into the c1c2 array */
 		count_t c1c2offset = 0u;
@@ -92,9 +92,9 @@ struct netlist_6502_static_state_type
 				const auto cond1 = !one_of<vss, vcc> (c1);
 				const auto cond2 = !one_of<vss, vcc> (c2);
 
-				if (cond1) nodes_dependant[0][nindex].push_unique (c1);
-				if (cond2) nodes_dependant[0][nindex].push_unique (c2);
-				nodes_dependant[1] [nindex].push_unique (cond1 ? c1 : c2);
+				if (cond1) nodes_dependant [0][nindex].push_unique (c1);
+				if (cond2) nodes_dependant [0][nindex].push_unique (c2);
+				nodes_dependant [1][nindex].push_unique (cond1 ? c1 : c2);
 			}
 		}
 	}
@@ -119,7 +119,7 @@ void initialize_state (state_type& state)
 {
 	using namespace node_names;
 
-	/* allocate state */	
+	/* allocate state */
 	state.nodes_pullup = netlist_6502_node_is_pullup;
 	state.nodes_pulldown.clear ();
 	state.nodes_value.clear ();
@@ -132,7 +132,7 @@ void initialize_state (state_type& state)
 }
 
 
-static inline void 
+static inline void
 group_add_node (state_type& state, nodenum_t n)
 {
 	/*
@@ -140,12 +140,14 @@ group_add_node (state_type& state, nodenum_t n)
 	 * with the same value - just because they all derive their value from
 	 * the fact that they are connected to vcc or vss.
 	 */
-	if (n == node_names::vss) {
+	if (n == node_names::vss)
+	{
 		state.group_contains_value = contains_vss;
 		return;
 	}
 
-	if (n == node_names::vcc)	{
+	if (n == node_names::vcc)
+	{
 		if (state.group_contains_value != contains_vss)
 			state.group_contains_value = contains_vcc;
 		return;
@@ -158,16 +160,16 @@ group_add_node (state_type& state, nodenum_t n)
 
 	switch (state.group_contains_value)
 	{
-	case contains_nothing:	if (state.nodes_pulldown	.get (n)) inplace_max (state.group_contains_value, contains_pulldown);
-	case contains_hi:				if (state.nodes_pullup		.get (n))	inplace_max (state.group_contains_value, contains_pullup);
-	case contains_pullup:		if (state.nodes_value			.get (n))	inplace_max (state.group_contains_value, contains_hi);
+	case contains_nothing:	if (state.nodes_pulldown.get (n)) inplace_max (state.group_contains_value, contains_pulldown);
+	case contains_hi:				if (state.nodes_pullup.get (n))	inplace_max (state.group_contains_value, contains_pullup);
+	case contains_pullup:		if (state.nodes_value.get (n))	inplace_max (state.group_contains_value, contains_hi);
 	default:
 		break;
 	}
 
 	/* revisit all transistors that control this node */
 	auto&& offset = st_state.nodes_c1c2offset;
-	for (auto&& tindex : range (offset[n], offset[n + 1]))
+	for (auto&& tindex : range (offset [n], offset [n + 1]))
 	{
 		auto c = st_state.nodes_c1c2s [tindex];
 		/* if the transistor connects c1 and c2... */
@@ -212,7 +214,7 @@ recalculate_node (state_type& state, nodenum_t node)
 		state.nodes_value.set (nindex, new_value);
 		for (auto&& tindex : st_state.nodes_gates [nindex])
 			state.trans_state.set (tindex, new_value);
-		for (auto&& nindex : st_state.nodes_dependant[new_value][nindex])
+		for (auto&& nindex : st_state.nodes_dependant [new_value][nindex])
 			state.list [state.out].insert (nindex);
 	}
 }
@@ -221,8 +223,8 @@ static inline void
 recalculate_node_list (state_type& state)
 {
 	/* loop limiter */
-	for (auto j : range(0, 20))
-	{			
+	for (auto j : range (0, 20))
+	{
 		/*
 		 * make the secondary list our primary list, use
 		 * the data storage of the primary list as the
@@ -249,12 +251,12 @@ recalculate_node_list (state_type& state)
 static inline void
 stabilize_chip (state_type& state)
 {
-	for (auto index: range(0, netlist_6502_node_count))
+	for (auto index : range (0, netlist_6502_node_count))
 		state.list [state.out].insert (index);
 	recalculate_node_list (state);
 }
 
-static inline void 
+static inline void
 set_node (state_type& state, nodenum_t index, bool value)
 {
 	state.nodes_pullup.set (index, value);
@@ -264,75 +266,75 @@ set_node (state_type& state, nodenum_t index, bool value)
 	recalculate_node_list (state);
 }
 
-static inline bool 
-get_node(const state_type& state, nodenum_t index)
+static inline bool
+get_node (const state_type& state, nodenum_t index)
 {
-	return state.nodes_value.get(index);
+	return state.nodes_value.get (index);
 }
 
 template <auto... _Index, typename _Value>
-static inline void 
+static inline void
 write_nodes (state_type& state, _Value value)
 {
 	if constexpr (sizeof ... (_Index) == 1)
-		set_node(state, _Index..., value);
+		set_node (state, _Index..., value);
 	else
-	for(const auto index: { _Index ... })
-	{
-		set_node (state, index, value & 1u);
-		value >>= 1u;
-	}
+		for (const auto index : { _Index ... })
+		{
+			set_node (state, index, value & 1u);
+			value >>= 1u;
+		}
 }
 
 template <auto... _Index, typename _Value>
-static inline void 
+static inline void
 read_nodes (const state_type& state, _Value& value)
 {
 	if constexpr (sizeof ... (_Index) == 1)
-		value = get_node(state, _Index...);
+		value = get_node (state, _Index...);
 	else
 	{
-	static constexpr auto q = sizeof...(_Index) - 1u;
-	for(const auto index: { _Index ... })
-	{
-		value >>= 1u;
-		value |= (get_node (state, index) << q);	
-	}	
-}
+		static constexpr auto q = sizeof...(_Index) - 1u;
+		for (const auto index : { _Index ... })
+		{
+			value >>= 1u;
+			value |= (get_node (state, index) << q);
+		}
+	}
 }
 
 template <typename _Value, auto... _Index>
-static inline auto 
+static inline auto
 read_nodes (const state_type& state) -> _Value
 {
-	_Value value { 0u };
-	read_nodes<_Index...>(state, value);
+	_Value value{ 0u };
+	read_nodes<_Index...> (state, value);
 	return value;
 }
 
-static inline void 
+static inline void
 init_and_reset_chip (state_type& state)
 {
-	bool clk { false };
+	bool clk{ false };
 	using namespace node_names;
 	/* set up data structures for efficient emulation */
-		
+
 	initialize_state (state);
 
-	set_node (state, res,		0);
-	set_node (state, clk0,	1);
-	set_node (state, rdy,		1);
-	set_node (state, so,		0);
-	set_node (state, irq,		1);
-	set_node (state, nmi,		1);
+	set_node (state, res, 0);
+	set_node (state, clk0, 1);
+	set_node (state, rdy, 1);
+	set_node (state, so, 0);
+	set_node (state, irq, 1);
+	set_node (state, nmi, 1);
 
 	stabilize_chip (state);
 }
 
 netlist_6502::netlist_6502 ()
-:	state { std::make_unique<state_type>() }
-{ 
-	init_and_reset_chip(*state); 
+	: state{ std::make_unique<state_type> () }
+{
+	init_and_reset_chip (*state);
 }
 
 netlist_6502::~netlist_6502 ()
@@ -340,98 +342,98 @@ netlist_6502::~netlist_6502 ()
 
 
 void netlist_6502::eval ()
-{ 
+{
 	recalculate_node_list (*state);
 }
 
 auto netlist_6502::get (bits _bits) const -> uint16_t
 {
 	using namespace node_names;
-	switch(_bits)
+	switch (_bits)
 	{
 	case bits::bus_addr:
-		return read_nodes<uint16_t, ab0, ab1, ab2, ab3, ab4, ab5, ab6, ab7, ab8, ab9, ab10, ab11, ab12, ab13, ab14, ab15>(*state);
+		return read_nodes<uint16_t, ab0, ab1, ab2, ab3, ab4, ab5, ab6, ab7, ab8, ab9, ab10, ab11, ab12, ab13, ab14, ab15> (*state);
 	case bits::bus_data:
-		return read_nodes<uint8_t, db0, db1, db2, db3, db4, db5, db6, db7>(*state);
+		return read_nodes<uint8_t, db0, db1, db2, db3, db4, db5, db6, db7> (*state);
 	case bits::bus_nmi:
-		return read_nodes<uint8_t, nmi>(*state);
+		return read_nodes<uint8_t, nmi> (*state);
 	case bits::bus_reset:
-		return read_nodes<uint8_t, res>(*state);
+		return read_nodes<uint8_t, res> (*state);
 	case bits::bus_irq:
-		return read_nodes<uint8_t, irq>(*state);
+		return read_nodes<uint8_t, irq> (*state);
 	case bits::bus_rw:
-		return read_nodes<uint8_t, rw>(*state);
+		return read_nodes<uint8_t, rw> (*state);
 	case bits::bus_sync:
-		return read_nodes<uint8_t, sync_>(*state);
+		return read_nodes<uint8_t, sync_> (*state);
 	case bits::bus_clock:
-		return read_nodes<uint8_t, clk0>(*state);
+		return read_nodes<uint8_t, clk0> (*state);
 	case bits::bus_ready:
-		return read_nodes<uint8_t, rdy>(*state);
-	case bits::reg_a:	
-		return read_nodes<uint8_t, a0, a1, a2, a3, a4, a5, a6, a7>(*state);
+		return read_nodes<uint8_t, rdy> (*state);
+	case bits::reg_a:
+		return read_nodes<uint8_t, a0, a1, a2, a3, a4, a5, a6, a7> (*state);
 	case bits::reg_x:
-		return read_nodes<uint8_t, x0, x1, x2, x3, x4, x5, x6, x7>(*state);
+		return read_nodes<uint8_t, x0, x1, x2, x3, x4, x5, x6, x7> (*state);
 	case bits::reg_y:
-		return read_nodes<uint8_t, y0, y1, y2, y3, y4, y5, y6, y7>(*state);
+		return read_nodes<uint8_t, y0, y1, y2, y3, y4, y5, y6, y7> (*state);
 	case bits::reg_s:
-		return read_nodes<uint8_t, s0, s1, s2, s3, s4, s5, s6, s7>(*state);
+		return read_nodes<uint8_t, s0, s1, s2, s3, s4, s5, s6, s7> (*state);
 	case bits::reg_p:
-		return read_nodes<uint8_t, p0, p1, p2, p3, p4, p5, p6, p7>(*state);
+		return read_nodes<uint8_t, p0, p1, p2, p3, p4, p5, p6, p7> (*state);
 	case bits::reg_pc:
-		return read_nodes<uint16_t, pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7, pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7>(*state);
+		return read_nodes<uint16_t, pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7, pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7> (*state);
 	case bits::reg_pch:
-		return read_nodes<uint8_t, pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7>(*state);
+		return read_nodes<uint8_t, pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7> (*state);
 	case bits::reg_pcl:
-		return read_nodes<uint8_t, pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7>(*state);
+		return read_nodes<uint8_t, pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7> (*state);
 	case bits::reg_ir:
-		return read_nodes<uint8_t, notir0, notir1, notir2, notir3, notir4, notir5, notir6, notir7>(*state)^0xffu;
+		return read_nodes<uint8_t, notir0, notir1, notir2, notir3, notir4, notir5, notir6, notir7> (*state) ^ 0xffu;
 	default:
-		throw std::runtime_error("Not implemented.");
+		throw std::runtime_error ("Not implemented.");
 	}
 }
 
 void netlist_6502::set (bits _bits, uint16_t val)
-{ 
+{
 	using namespace node_names;
-	switch(_bits)
+	switch (_bits)
 	{
 	case bits::bus_addr:
-		return write_nodes<ab0, ab1, ab2, ab3, ab4, ab5, ab6, ab7, ab8, ab9, ab10, ab11, ab12, ab13, ab14, ab15>(*state, val);
+		return write_nodes<ab0, ab1, ab2, ab3, ab4, ab5, ab6, ab7, ab8, ab9, ab10, ab11, ab12, ab13, ab14, ab15> (*state, val);
 	case bits::bus_data:
-		return write_nodes<db0, db1, db2, db3, db4, db5, db6, db7>(*state, val);
+		return write_nodes<db0, db1, db2, db3, db4, db5, db6, db7> (*state, val);
 	case bits::bus_nmi:
-		return write_nodes<nmi>(*state, val);
+		return write_nodes<nmi> (*state, val);
 	case bits::bus_reset:
-		return write_nodes<res>(*state, val);
+		return write_nodes<res> (*state, val);
 	case bits::bus_irq:
-		return write_nodes<irq>(*state, val);
+		return write_nodes<irq> (*state, val);
 	case bits::bus_rw:
-		return write_nodes<rw>(*state, val);
+		return write_nodes<rw> (*state, val);
 	case bits::bus_sync:
-		return write_nodes<sync_>(*state, val);
+		return write_nodes<sync_> (*state, val);
 	case bits::bus_clock:
-		return write_nodes<clk0>(*state, val);
+		return write_nodes<clk0> (*state, val);
 	case bits::bus_ready:
-		return write_nodes<rdy>(*state, val);
-	case bits::reg_a:	
-		return write_nodes<a0, a1, a2, a3, a4, a5, a6, a7>(*state, val);
+		return write_nodes<rdy> (*state, val);
+	case bits::reg_a:
+		return write_nodes<a0, a1, a2, a3, a4, a5, a6, a7> (*state, val);
 	case bits::reg_x:
-		return write_nodes<x0, x1, x2, x3, x4, x5, x6, x7>(*state, val);
+		return write_nodes<x0, x1, x2, x3, x4, x5, x6, x7> (*state, val);
 	case bits::reg_y:
-		return write_nodes<y0, y1, y2, y3, y4, y5, y6, y7>(*state, val);
+		return write_nodes<y0, y1, y2, y3, y4, y5, y6, y7> (*state, val);
 	case bits::reg_s:
-		return write_nodes<s0, s1, s2, s3, s4, s5, s6, s7>(*state, val);
+		return write_nodes<s0, s1, s2, s3, s4, s5, s6, s7> (*state, val);
 	case bits::reg_p:
-		return write_nodes<p0, p1, p2, p3, p4, p5, p6, p7>(*state, val);
+		return write_nodes<p0, p1, p2, p3, p4, p5, p6, p7> (*state, val);
 	case bits::reg_pc:
-		return write_nodes<pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7, pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7>(*state, val);
+		return write_nodes<pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7, pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7> (*state, val);
 	case bits::reg_pch:
-		return write_nodes<pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7>(*state, val);
+		return write_nodes<pch0, pch1, pch2, pch3, pch4, pch5, pch6, pch7> (*state, val);
 	case bits::reg_pcl:
-		return write_nodes<pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7>(*state, val);
+		return write_nodes<pcl0, pcl1, pcl2, pcl3, pcl4, pcl5, pcl6, pcl7> (*state, val);
 	case bits::reg_ir:
-		return write_nodes<notir0, notir1, notir2, notir3, notir4, notir5, notir6, notir7>(*state, uint8_t (val^0xffu));
+		return write_nodes<notir0, notir1, notir2, notir3, notir4, notir5, notir6, notir7> (*state, uint8_t (val ^ 0xffu));
 	default:
-		throw std::runtime_error("Not implemented.");
+		throw std::runtime_error ("Not implemented.");
 	}
 }
